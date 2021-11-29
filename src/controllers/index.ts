f3df91f1dd498dc698cee9bf00bc6087e194fa55
Kmilo <KmiloCrow27@gmail.com>
2021-11-28 19:43:05 -0500

    Se agrega controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './factura-ventas.controller';
 export * from './ventas-factura.controller';
+export * from './venta.controller';
+export * from './factura.controller';
